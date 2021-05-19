641f37e49b86edea87c13575767e826edec48f1c
Daniel Waite <danielwaite6@hotmail.com>
2021-05-19 02:56:44 -0300

    first commit

---
+++ lib/create_note_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class CreateNotePage extends StatefulWidget {
+  @override
+  _CreateNotePageState createState() => _CreateNotePageState();
+}
+
+class _CreateNotePageState extends State<CreateNotePage> {
+  var description = "";
+  var isEdit = false;
+  var textController = TextEditingController();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      if (ModalRoute.of(context)!.settings.arguments != null) {
+        description = ModalRoute.of(context)!.settings.arguments as String;
+        textController.text = description;
+        isEdit = true;
+        setState(() {});
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isEdit ? "Edit Note" : "Create Note"),
+        centerTitle: true,
+        actions: [
+          if (isEdit)
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                Navigator.pop(context, "");
+              },
+            ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textController,
+              maxLines: null,
+              onChanged: (value) {
+                description = value;
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                labelText: "Descrição",
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            if (description.isNotEmpty)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context, description);
+                      },
+                      child: Text("Salvar"),
+                    ),
+                  ),
+                ],
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
